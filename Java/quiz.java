c29c379ef9b4dcda0fc12920cdb4b2b04015acef
Abby <ajjon037@learner.dvusd.org>
2022-08-18 11:39:21 -0700

    priest thursday

---
+++ Java/quiz.java
@@ -0,0 +1,15 @@
+
+
+public class quiz
+{
+ 
+    public static void main(String args[])
+    {
+       System.out.println("How many dogs have died in your life? A. All");
+       System.out.println("How many years have you loved priest? A.Forever B.10 C");
+       System.out.println("Priest? A.Yes B.Yes C.Yes D.Yes");
+    }
+
+ 
+    
+}
